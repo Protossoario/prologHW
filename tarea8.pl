033e18f6181fbe6d49edddfbacfcd54bbab0285b
Lalo <lalo.sa.94@gmail.com>
2014-11-25 15:07:06 -0600

    Problema 2-a resuelto

--- tarea8.pl
+++ tarea8.pl
@@ -1,3 +1,4 @@
+% Grafo g
 nodo(g, a).
 nodo(g, b).
 nodo(g, c).
@@ -11,5 +12,8 @@ arco(g, c, a, 12).
 arco(g, c, d, 6).
 arco(g, d, e, 7).
 arco(g, e, c, 3).
-costo(Grafo, [Nodo1, Nodo2], Costo) :- arco(Grafo, Nodo1, Nodo2, Costo).
-costo(Grafo, [Nodo1, Nodo2 | Ruta], Costo) :- arco(Grafo, Nodo1, Nodo2, Costo1), costo(Grafo, [Nodo2 | Ruta], Costo2), Costo is Costo1 + Costo2.
+
+% Un solo nodo en la ruta.
+costo(_, [_], 0) :- !.
+% El costo total es la suma del costo entre los dos nodos siguientes mas el costo del resto de la ruta a partir del segundo nodo.
+costo(Grafo, [Nodo1, Nodo2 | Ruta], Costo) :- arco(Grafo, Nodo1, Nodo2, Costo1), costo(Grafo, [Nodo2 | Ruta], Costo2), !, Costo is Costo1 + Costo2.
