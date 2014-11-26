9ecce71dc68d1af3aab8bc167c066087934830c3
Lalo <lalo.sa.94@gmail.com>
2014-11-25 20:26:46 -0600

    Pregunta 2-b resulta

--- tarea8.pl
+++ tarea8.pl
@@ -17,3 +17,10 @@ arco(g, e, c, 3).
 costo(_, [_], 0) :- !.
 % El costo total es la suma del costo entre los dos nodos siguientes mas el costo del resto de la ruta a partir del segundo nodo.
 costo(Grafo, [Nodo1, Nodo2 | Ruta], Costo) :- arco(Grafo, Nodo1, Nodo2, Costo1), costo(Grafo, [Nodo2 | Ruta], Costo2), !, Costo is Costo1 + Costo2.
+
+% Obtiene la lista de nodos del grafo y se la pasa a la funcion auxiliar.
+arcos(Grafo, L) :- findall(X, nodo(Grafo, X), Nodos), cuenta_arcos(Grafo, Nodos, L).
+
+% Recibe el grafo y una lista de nodos; regresa una lista con los nodos y la cantidad de arcos que tienen dentro del grafo.
+cuenta_arcos(_, [], []).
+cuenta_arcos(Grafo, [Nodo|Resto], Lista) :- findall(X, arco(Grafo, Nodo, X, _), Arcos), cuenta_arcos(Grafo, Resto, Lista2), !, length(Arcos, CantArcos), Lista = [[Nodo, CantArcos] | Lista2].
